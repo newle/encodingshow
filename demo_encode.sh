e2eea4e3ababbc2bef31a4caf9e707bdfa63fd0f
newle <newle.hit@gmail.com>
2015-05-26 22:41:28 +0800

    nothing change

--- demo_encode.sh
+++ demo_encode.sh
@@ -2,13 +2,18 @@
 
 if [ $# -ne 2 ]; then
 	echo "example: sh demo_encode.sh ÄãºÃ°¡ `date +\"%s\"`"
+	echo "example: sh demo_encode.sh "utf16\#\#\#\#0xFEFF 0x5E05 0x54E5" `date +\"%s\"`"
 	exit
 fi
 
 text="$1"
 prefix="$2"
 
-./encoding raw "$text" > $prefix"norm.log"
+if [ "${text%%#*}" == "${text##*#}" ]; then 
+	./encoding raw "$text" > $prefix"norm.log"
+else 
+	./encoding "${text%%#*}" "${text##*#}" > $prefix"norm.log"
+fi
 
 echo $text"	"$prefix >> encoding_process.log
 
